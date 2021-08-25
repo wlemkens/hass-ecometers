d262815e773d45bdbfd6dc36722731b5bd0f43c0
Wim Lemkens <wim.lemkensmail.com>
2021-08-25 15:22:55 +0200

    Put in the example sensor

--- sensor.py
+++ sensor.py
@@ -27,77 +27,26 @@ import homeassistant.helpers.config_validation as cv
 from .ecometers.ecometers.ecometers import EcoMeterS
 # import homeassistant.config as config
 
-_LOGGER = logging.getLogger(__name__)
-
-DEFAULT_PORT = '/dev/ttyUSB0'
-
-CONF_PORT = "serial_port"
-CONF_HEIGHT = "height"
-CONF_OFFSET = "offset"
-
-# Helper classes for declaring sensor configurations
-
-class EcoMeterSSensorConfig:
-    """
-    Eco Meter S Sensor Configuration.
-    """
-
-    def __init__(self, friendly_name, port, height, offset):
-        """Constructor.
-        Args:
-            friendly_name (string|func): Friendly name
-            port (string): serial port
-            height (int): height of the tank in cm
-            offset (int): offset of the sensor in cm
-        """
-        self.friendly_name = friendly_name
-        self.port = port
-        self.height = height
-        self.offset = offset
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.helpers.entity import Entity
 
 
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_PORT): cv.string,
-    vol.Required(CONF_HEIGHT): cv.positive_int,
-    vol.Required(CONF_OFFSET): cv.positive_int
-})
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the sensor platform."""
+    add_entities([ExampleSensor()])
 
 
-async def async_setup_platform(hass: HomeAssistantType, config: ConfigType,
-                               async_add_entities, discovery_info=None):
-    """Set up the WUnderground sensor."""
-    port = config.get(CONF_PORT, DEFAULT_PORT)
-    height = config.get(CONF_HEIGHT)
-    offset = config.get(CONF_OFFSET)
-    _LOGGER.warning("Config, port = %s, heigt = %d, offset = %d",
-                            self._port, self.height, self.offset)
+class ExampleSensor(Entity):
+    """Representation of a Sensor."""
 
-
-    ecometers = EcoMeterS(port, height, offset)
-
-    sensors = []
-    sensors.append(EcoMeterSLevelSensor(ecometers,
-                                          unique_id_base))
-
-    async_add_entities(sensors, True)
-
-
-class EcoMeterSSensor(Entity):
-    """Implementing the Eco Meter S sensor."""
-
-    def __init__(self, ecometers: EcoMeterS,
-                 unique_id_base: str):
+    def __init__(self):
         """Initialize the sensor."""
-        self.ecometers = ecometers
-        self.ecometers.add_on_data_received_callback(self.on_data_received)
         self._state = None
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name
-        #return self._cfg_expand("friendly_name")
+        return 'Example Temperature'
 
     @property
     def state(self):
@@ -105,23 +54,13 @@ class EcoMeterSSensor(Entity):
         return self._state
 
     @property
-    def icon(self):
-        """Return icon."""
-        return self._icon
-
-    @property
     def unit_of_measurement(self):
-        """Return the units of measurement."""
-        return self._unit_of_measurement
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
 
-class EcoMeterSLevelSensor(EcoMeterSSensor):
-    def __init__(self, ecometers: EcoMeterS,
-                unique_id_base: str):
-        self._name = "Tank level"
-        self._unit_of_measurement = LENGTH_CENTIMETERS
-        self._icon = "mdi:hydraulic-oil-level"
-        self._icon = "mdi:arrow-expand-vertical"
+    def update(self):
+        """Fetch new state data for the sensor.
 
-    def on_data_received(self, ecometers: EcoMeterS):
-        self._state = ecometers.level
-
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = 23
